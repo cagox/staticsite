9d772cee4c9f0807c57eac42077901162b60dcb8
burlingk <cagox@cagox.net>
2025-02-28 21:14:26 +0900

    Initial commit.
    We have the bare basics going, and we haave created the
    TextNode class and the TextType Enum.

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from textnode import TextNode
+from textnode import TextType
+
+def main():
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev/")
+    print(node) 
+
+
+
+
+
+
+main()
