dcd441af5751220848bfae491a549a0238591d9f
burlingk <cagox@cagox.net>
2025-03-01 17:02:21 +0900

    split_node_delimiter added.
    Probnably need more tests though.

--- src/nodeuitils.py
+++ src/nodeuitils.py
@@ -1,5 +1,4 @@
-from textnode import TextNode
-from textnode import TextType
+from textnode import TextNode, TextType
 from leafnode import LeafNode
 
 
@@ -25,3 +24,23 @@ def text_node_to_html_node(text_node):
         case _:
             raise TypeError("No such type")
 
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimiter)
+        if len(sections) % 2 == 0:
+            raise ValueError("invalid markdown, formatted section not closed")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
--- src/test_nodeutils.py
+++ src/test_nodeutils.py
@@ -3,9 +3,20 @@ from nodeuitils import *
 from htmlnode import *
 from textnode import *
 
-class TestNodeUtils(unittest.TestCase):
+class TestNodeToHTMLNode(unittest.TestCase):
     def test_text(self):
         node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_split(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [
+                    TextNode("This is text with a ", TextType.TEXT),
+                    TextNode("code block", TextType.CODE),
+                    TextNode(" word", TextType.TEXT),
+                ]
+        self.assertEqual(new_nodes, expected)
